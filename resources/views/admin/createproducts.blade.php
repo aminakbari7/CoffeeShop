89f4f9fab2f6db30cd44eb7e67d9a80fc0332c5b
aminakbari7 <aminakbari733@gmail.com>
2024-02-03 22:10:18 +0330

    Create createproducts.blade.php

---
+++ resources/views/admin/createproducts.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
+    <title>Admin Panel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+     <link href="{{ asset('adminstyle/styles/style.css') }}" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <x-app-layout>
+  </x-app-layout>
+  <div dir="rtl" id="wrapper" style="margin-top: 100px;">
+    <div class="container-fluid">
+       <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-5 d-inline">Create Product</h5>
+          <form method="POST" action="" enctype="multipart/form-data">
+                <!-- Email input -->
+                <div class="form-outline mb-4 mt-4">
+                  <input type="text" name="name" id="form2Example1" class="form-control" placeholder="name" />
+                 
+                </div>
+                <div class="form-outline mb-4 mt-4">
+                  <input type="text" name="price" id="form2Example1" class="form-control" placeholder="price" />
+                 
+                </div>
+                <div class="form-outline mb-4 mt-4">
+                  <input type="file" name="image" id="form2Example1" class="form-control"  />
+                 
+                </div>
+                <div class="form-group">
+                  <label for="exampleFormControlTextarea1">Description</label>
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+               
+                <div class="form-outline mb-4 mt-4">
+
+                  <select name="price" class="form-select  form-control" aria-label="Default select example">
+                    <option selected>Choose Type</option>
+                    <option value="drink">drink</option>
+                    <option value="dessert">dessert</option>
+                  </select>
+                </div>
+
+                <br>
+              
+
+      
+                <!-- Submit button -->
+                <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">create</button>
+
+          
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
