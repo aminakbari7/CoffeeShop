ed6438a47c98e0a8b2bd76698a7a3a5e59bf0b1f
aminakbari7 <aminakbari733@gmail.com>
2024-02-07 09:12:06 +0330

    Create Addcart.php

---
+++ app/Livewire/Addcart.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Cart\Cart;
+use App\Models\Product\Product;
+use Illuminate\Support\Facades\Session;
+
+
+class Addcart extends Component
+{
+ 
+    public $product_id="";
+    public function save()
+    {
+
+        $product=Product::find($this->product_id);
+        $addcart=Cart::Create(['user_id'=>auth()->user()->id,'product_id'=>$product->id,
+       'image'=>$product->image,
+        'price'=>$product->price,
+        'name'=>$product->name]);
+        Session()->flash('msg', 'اضافه شد!'); 
+
+    }
+    
+
+    public function mount($product_id)
+    {
+    $this->product_id = $product_id;
+    }
+    public function render()
+    {
+        return view('livewire.addcart');
+    }
+}
