9bd4ab588a23b970184a5ec4d952a4ca0c160ceb
aminakbari7 <aminakbari733@gmail.com>
2024-02-07 17:50:46 +0330

    Update Delcart.php

--- app/Livewire/Delcart.php
+++ app/Livewire/Delcart.php
@@ -1,23 +1,34 @@
 <?php
 
 namespace App\Livewire;
-
+use Livewire\WithPagination;
 use Livewire\Component;
 use App\Models\Cart\Cart;
 use App\Models\Product\Product;
 use Illuminate\Support\Facades\Session;
 class Delcart extends Component
 {
+    use WithPagination;
+    public  $cart;
+    public $totalprice;
+    public $del;
     public function delcart($id)
     {
-        $cart=cart::find($id);
-        $cart->delete();
-       
+      
+        Cart::destroy($id);
+       //$this->cart=cart::select()->where('user_id', Auth()->user()->id)->get();
+        $this->dispatch('update');
+      
+    }
+    public function mount()
+    {
+        $this->cart=cart::select()->where('user_id', Auth()->user()->id)->get();
+        $this->totalprice=cart::select()->where('user_id', Auth()->user()->id)->sum('price');
+
     }
     public function render()
     {
-        $totalprice=cart::select()->where('user_id', Auth()->user()->id)->sum('price');
-        $cart=cart::select()->where('user_id', Auth()->user()->id)->get();
-        return view('livewire.delcart',['cart'=>$cart,'totalprice'=>$totalprice]);
+        $this->mount();
+        return view('livewire.delcart');
     }
 }
