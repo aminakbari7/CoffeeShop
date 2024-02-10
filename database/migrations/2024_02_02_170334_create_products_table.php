82dd516401889a90abc6e5ed1090a268b4de47e0
aminakbari7 <aminakbari733@gmail.com>
2024-02-10 08:54:47 +0330

    Update 2024_02_02_170334_create_products_table.php

--- database/migrations/2024_02_02_170334_create_products_table.php
+++ database/migrations/2024_02_02_170334_create_products_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();;
             $table->string('image');
             $table->string('price');
-            $table->string('desription');
+            $table->string('description');
             $table->timestamps();
         });
     }
