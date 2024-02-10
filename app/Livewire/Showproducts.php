8ff8da55a8b0b03401b8c2aee678ab572754bfaa
aminakbari7 <aminakbari733@gmail.com>
2024-02-10 16:17:58 +0330

    Update Showproducts.php

--- app/Livewire/Showproducts.php
+++ app/Livewire/Showproducts.php
@@ -8,6 +8,19 @@ use Illuminate\Support\Facades\DB;
 use Livewire\WithPagination;
 class Showproducts extends Component
 {    use WithPagination;
+
+   public $pid;
+
+    public function delproduct($id)
+    {
+        $this->pid=$id;
+        product::destroy($this->pid);
+        Session()->flash('msg', 'حذف شد!'); 
+        $products = Product::orderBy('created_at', 'DESC')->paginate(8);
+        return view('livewire.showproducts',['products'=>$products]);
+    }
+
+
     public function render()
     {
        
