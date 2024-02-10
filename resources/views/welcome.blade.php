d93cc41f61395929635042f9a1247c5dff7aa064
aminakbari7 <aminakbari733@gmail.com>
2024-02-10 08:54:58 +0330

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -234,10 +234,10 @@
 			@foreach ($products as $product)
 			<div class="col-md-3">
 				<div class="menu-entry">
-    					<a href="#" class="img" style="background-image: url({{asset('assets/images/'.$product->image)}});"></a>
+    					<a href="#" class="img" style="background-image: url({{asset('storage/images/'.$product->image)}});"></a>
     					<div class="text text-center pt-4">
     						<h3><a href="{{route('product.showsingle' ,$product->id)}}">{{$product->name  }}</a></h3>
-    						<p>{{$product->desription  }}</p>
+    						<p>{{$product->description  }}</p>
     						<p class="price"><span>{{$product->price }}</span></p>
     						<p><a href="{{route('product.showsingle' ,$product->id)}}"class="btn btn-primary btn-outline-primary">Show</a></p>
     					</div>
