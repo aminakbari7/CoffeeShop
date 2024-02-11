608d750c612e82109fa62f940a46cdbd14d70950
aminakbari7 <aminakbari733@gmail.com>
2024-02-11 15:50:28 +0330

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,11 +1,33 @@
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
-      <a class="navbar-brand" href="{{route('index')}}">Coffee<small>Blend</small></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="oi oi-menu"></span> Menu
          </button>
+
+
       <div class="collapse navbar-collapse" id="ftco-nav">
         <ul class="navbar-nav ml-auto">
+
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               <strong> search</strong>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <div class="dropdown-item" id="navbarSupportedContent">
+                  <form class="form-inline my-2 my-lg-0" style="display:inline-block;">
+                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </div>
+              </div>
+          </li>
+
+
+
+
+
+
           <li class="nav-item active"><a href="{{route('index')}}" class="nav-link">Home</a></li>
           <li class="nav-item active"><a href="{{route('showp')}}" class="nav-link">products</a></li>
 
