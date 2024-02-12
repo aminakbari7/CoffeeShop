d1769cf25d5e9dafe13978cfbffbba5e592dc79c
aminakbari7 <aminakbari733@gmail.com>
2024-02-12 15:53:26 +0330

    Update showproducts.blade.php

--- resources/views/livewire/showproducts.blade.php
+++ resources/views/livewire/showproducts.blade.php
@@ -6,13 +6,13 @@
 			@foreach ($products as $product)
 			<div class="col-md-3">
 				<div class="menu-entry">
-    					<a href="#" class="img" style="background-image: url({{asset('storage/images/'.$product->image)}});"></a>
+    					<a href="{{route('product.showsingle' ,$product->id)}}" class="img" style="background-image: url({{asset('storage/images/'.$product->image)}});"></a>
     					<div class="text text-center pt-4">
     						<h3><a href="{{route('product.showsingle' ,$product->id)}}">{{$product->name  }}</a></h3>
     						<p>{{$product->description  }}</p>
     						<p class="price"><span>{{$product->price }}</span></p>
     						<p><a href="{{route('product.showsingle' ,$product->id)}}"class="btn btn-primary btn-outline-primary">Show</a></p>
-							<p><a class="btn btn-primary btn-outline-primary" wire:click="delproduct({{ $product->id }})">حذف</a></p>
+							<p><a class="btn btn-primary btn-outline-primary" wire:key="product-{{ $product->id }}" wire:click="delproduct({{ $product->id }})">حذف</a></p>
 
     					</div>
     				</div>
