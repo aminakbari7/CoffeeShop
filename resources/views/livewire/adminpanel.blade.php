2c4b8b1eb85e193dfb6e851bcdbc6965d0a8f245
aminakbari7 <aminakbari733@gmail.com>
2024-02-13 23:42:20 +0330

    Update adminpanel.blade.php

--- resources/views/livewire/adminpanel.blade.php
+++ resources/views/livewire/adminpanel.blade.php
@@ -3,43 +3,36 @@
     <div class="row">
       <div class="col-md-3">
         <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Products</h5>
-            <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-            <p class="card-text">number of products: {{ $productsize }}</p>
-           
-          </div>
+            <a href="{{route('showp')}}">
+                 <div class="card-body">
+                    <h5 class="card-title">Products</h5>
+                    <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
+                    <p class="card-text">number of products: {{ $productsize }}</p>
+                </div>
+             </a>
         </div>
       </div>
       <div class="col-md-3">
         <div class="card">
-          <div class="card-body">
+            <a href="{{route('showusers')}}"> <div class="card-body">
             <h5 class="card-title">users</h5>
             
             <p class="card-text">number of users: {{ $usersize }}</p>
             
           </div>
+        </a>
         </div>
       </div>
       <div class="col-md-3">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Bookings</h5>
-            
-            <p class="card-text">number of bookings: 4</p>
-            
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card">
-          <div class="card-body">
+          <div class="card">
+            <a href="{{route('listadmin')}}"><div class="card-body">
             <h5 class="card-title">Admins</h5>
             
             <p class="card-text">number of admins: {{ $adminsize }}</p>
             
           </div>
         </div>
+    </a>
       </div>
     </div>
 </div>
