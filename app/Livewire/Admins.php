3154d8dfc76dab44073b22b65d064a37cedc0b50
aminakbari7 <aminakbari733@gmail.com>
2024-02-14 08:45:25 +0330

    Update Admins.php

--- app/Livewire/Admins.php
+++ app/Livewire/Admins.php
@@ -3,17 +3,50 @@
 namespace App\Livewire;
 use App\Models\user;
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
+use Livewire\WithPagination;
 
 class Admins extends Component
 {
+    use WithPagination;
 
    
-       
-public function mount(){
-  
-  
+    public $searchuser="";
+    public $uid;
+    public $key=-1;
+    public $upid=0;
+    public $newname;
+    public $newemail;
+    public function searchu()
+    {
+        $this->resetPage(); 
 
-}
+
+    }
+    public function save($id){
+        $user=User::find($id);
+        if($this->newemail!=null)
+        {
+            $user->email = $this->newemail;
+        }
+        if($this->newname!=null)
+        {
+            $user->name= $this->newname;
+        }
+        if($this->newname!=null || $this->newemail!=null)
+        {
+            $user->save();
+        }
+       
+        $this->uid=0;
+        $this->key =-1;
+        $userss=user::orderBy('created_at', 'DESC')->paginate(4);
+        return view('livewire.adminuser',['userss'=>$userss]);
+    }
+   public function searchuser()
+   {
+
+   }
     public function render()
     {
         $adminss = User::select()->where('role', 'admin')->get();
