9b1e3165c2907d4cf73aa94616b123d0a6dc4c14
aminakbari7 <aminakbari733@gmail.com>
2024-02-18 20:54:27 +0330

    Update Commentcheck.php

--- app/Livewire/Commentcheck.php
+++ app/Livewire/Commentcheck.php
@@ -14,7 +14,35 @@ class Commentcheck extends Component
     public $size=0;
     public $comments;
     public $delid;
-
+    public $keyu=-1;
+    public $newbody="";
+    public $idu=-1;
+    public function updatec($comment_id)
+    {
+        $this->keyu=1;
+        $this->idu=$comment_id;
+        $this->size=comment::select()->where('product_id', $this->product_id)->count();
+        $this->comments=comment::select()->where('product_id', $this->product_id)->get();
+        return view('livewire.commentcheck');
+    }
+    public function editc($id)
+    {
+       
+        if($this->newbody!=null)
+        {
+            $temp=Comment::find($id);
+            $temp->body=$this->newbody;
+            $temp->save();
+            
+        }
+        
+        $this->keyu=-1;
+        $this->idu=-1;
+        $this->size=comment::select()->where('product_id', $this->product_id)->count();
+        $this->comments=comment::select()->where('product_id', $this->product_id)->get();
+        return view('livewire.commentcheck');
+        
+    }
     public function mount($product_id)
     {
     $this->product_id = $product_id;
