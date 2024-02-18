9829432b0526db47cef96be44a46c3f9c0f5cff5
aminakbari7 <aminakbari733@gmail.com>
2024-02-18 16:35:14 +0330

    Update Commentcheck.php

--- app/Livewire/Commentcheck.php
+++ app/Livewire/Commentcheck.php
@@ -13,37 +13,47 @@ class Commentcheck extends Component
     public $product_id="";
     public $size=0;
     public $comments;
+    public $delid;
 
     public function mount($product_id)
     {
-
-       
     $this->product_id = $product_id;
-    
-
     }
+    public function deletec($id)
+    {
+        $this->delid=$id;
+        comment::destroy($this->delid);
+        $this->size=comment::select()->where('product_id', $this->product_id)->count();
+        $this->comments=comment::select()->where('product_id', $this->product_id)->get();
+        return view('livewire.commentcheck');
+    }
+
+
+
     public function save($product_id)
     {
-    if($this->cbody!=null)
+
+    if($this->cbody!="")
     {
         comment::Create([
         'user_id'=>auth()->user()->id,
         'product_id'=>$product_id,
         'body'=>$this->cbody]);
+        $this->cbody="";
+        $this->size=comment::select()->where('product_id', $this->product_id)->count();
+    $this->comments=comment::select()->where('product_id', $this->product_id)->get();
+    return view('livewire.commentcheck');
 
     }
-    $this->cbody="";
-    $this->size=comment::select()->where('product_id', $this->product_id)->count();
-
-        $this->comments=comment::select()->where('product_id', $this->product_id)->get();
-
+    
+    
     }
 
 
     public function render()
     { 
+        $this->cbody="";
         $this->size=comment::select()->where('product_id', $this->product_id)->count();
-
         $this->comments=comment::select()->where('product_id', $this->product_id)->get();
         return view('livewire.commentcheck');
     }
