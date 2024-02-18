b06069238e6c8560d7052167b48943b031e84d87
aminakbari7 <aminakbari733@gmail.com>
2024-02-18 16:35:12 +0330

    Update commentcheck.blade.php

--- resources/views/livewire/commentcheck.blade.php
+++ resources/views/livewire/commentcheck.blade.php
@@ -1,7 +1,7 @@
-<section>
+
     <div class="container">
             <div class="col-sm-5 ">
-                <h1>Comments</h1>
+                <h1>Comments={{ $size }}</h1>
                  @if($size>0)
                     @foreach ($comments as  $comment)
                     <div class="row">
@@ -9,6 +9,12 @@
                         <img src="https://i.imgur.com/yTFUilP.jpg" alt="" class="rounded-circle" width="40" height="40">
                         <h4>Jhon Doe</h4>
                         <span>- 20 October, 2018</span>
+                        <button wire:key="comment-{{ $comment->id }}" wire:click="deletec({{ $comment->id }})" type="button" class="btn btn-outline-danger">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
+                            </svg>
+                          </button>
                         <br>
                         <p>{{ $comment->body }}</p>
                     </div>
@@ -16,12 +22,13 @@
                     @endforeach
                     @endif
         </div>
-        <div class="col-lg-4 " style="margin-top: 20px" >
+        <div class="col-lg-4 " style="margin-top: 20px;margin-top:80px" >
             <form wire:submit="save({{ $product_id }})">
                 <h4>Leave a comment</h4>
-                <textarea wire:model="cbody"  cols="20" rows="2" class="form-control" style="background-color: black;" placeholder="....."></textarea>
+                {{ $cbody }}
+                <textarea wire:model="cbody" name="cbody" cols="20" rows="2" class="form-control" style="background-color: black;" placeholder=".....">{{ $cbody }}</textarea>
                 <button type="submit"  name="submit" class="btn btn-outline-success my-2 my-sm-0" >send comment </button>
             </form>
         </div>
     </div>
-</section>
+
